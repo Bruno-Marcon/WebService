a51121926d56effb8b576c74b166a3cace5b3a9f
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2024-03-24 19:41:43 -0300

    upload exp2

--- experimento2/client/client.js
+++ experimento2/client/client.js
@@ -1,54 +1,41 @@
 const axios = require('axios');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 const wsdlUrl = 'http://localhost:3000/mdc';
 
-function fazerChamadaSOAP(x, y) {
-  const requestData = `
+// Dados de entrada para o serviço web
+const requestData = `
     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:mdc="http://localhost:3000/mdc">
         <soapenv:Header/>
         <soapenv:Body>
             <mdc:CalculateMDCRequest>
-                <x>${x}</x>
-                <y>${y}</y>
+                <x>1920</x>
+                <y>1080</y>
             </mdc:CalculateMDCRequest>
         </soapenv:Body>
     </soapenv:Envelope>
-  `;
+`;
 
-  const axiosConfig = {
+const axiosConfig = {
     headers: {
-      'Content-Type': 'text/xml;charset=UTF-8',
-      'SOAPAction': 'http://localhost:3000/mdc'
+        'Content-Type': 'text/xml;charset=UTF-8',
+        'SOAPAction': 'http://localhost:3000/mdc'
     }
-  };
+};
 
-  axios.post(wsdlUrl, requestData, axiosConfig)
+axios.post(wsdlUrl, requestData, axiosConfig)
     .then(response => {
-      const mdc = parseInt(response.data.match(/<MDC>(\d+)<\/MDC>/)[1]);
+        const mdc = parseInt(response.data.match(/<MDC>(\d+)<\/MDC>/)[1]);
+        
+        const x = 1920;
+        const y = 1080;
 
-      const aspectRatioX = x / mdc;
-      const aspectRatioY = y / mdc;
+        const aspectRatioX = x / mdc;
+        const aspectRatioY = y / mdc;
 
-      console.log('MDC:', mdc);
-      console.log('Aspect Ratio X:', aspectRatioX);
-      console.log('Aspect Ratio Y:', aspectRatioY);
-
-      rl.close();
+        console.log('MDC:', mdc);
+        console.log('Aspect Ratio X:', aspectRatioX);
+        console.log('Aspect Ratio Y:', aspectRatioY);
     })
     .catch(error => {
-      console.error('Erro ao chamar o serviço:', error);
-      rl.close();
+        console.error('Erro ao chamar o serviço:', error);
     });
-}
-
-rl.question('Digite o valor de x: ', (x) => {
-  rl.question('Digite o valor de y: ', (y) => {
-    fazerChamadaSOAP(parseInt(x), parseInt(y));
-  });
-});
