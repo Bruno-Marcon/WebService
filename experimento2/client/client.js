bd077c74d7a0ee45402f5856643a2bb14519deb5
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2024-03-24 18:21:40 -0300

    upload exp2

--- experimento2/client/client.js
+++ experimento2/client/client.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 
-// URL do serviço WSDL
 const wsdlUrl = 'http://localhost:3000/mdc';
 
 // Dados de entrada para o serviço web
@@ -16,7 +15,6 @@ const requestData = `
     </soapenv:Envelope>
 `;
 
-// Configurações para a chamada SOAP
 const axiosConfig = {
     headers: {
         'Content-Type': 'text/xml;charset=UTF-8',
@@ -24,21 +22,16 @@ const axiosConfig = {
     }
 };
 
-// Chamada ao serviço web
 axios.post(wsdlUrl, requestData, axiosConfig)
     .then(response => {
-        // Extrair MDC da resposta
         const mdc = parseInt(response.data.match(/<MDC>(\d+)<\/MDC>/)[1]);
         
-        // Dados de entrada para o cálculo do Aspect Ratio
         const x = 1920;
         const y = 1080;
 
-        // Calcular o Aspect Ratio
         const aspectRatioX = x / mdc;
         const aspectRatioY = y / mdc;
 
-        // Exibir resultados
         console.log('MDC:', mdc);
         console.log('Aspect Ratio X:', aspectRatioX);
         console.log('Aspect Ratio Y:', aspectRatioY);
